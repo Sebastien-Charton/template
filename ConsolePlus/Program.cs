aa1cb36b147163a4b94c8091787cd8110a9555c2
sebastien-charton <sebastiencharton@protonmail.com>
2020-10-06 23:11:39 +0200

    restore command added + controller file template

--- ConsolePlus/Program.cs
+++ ConsolePlus/Program.cs
@@ -7,8 +7,6 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 
-// DI, Serilog, Settings
-
 namespace ConsolePlus
 {
     class Program
---
+++ Controller/ControllerFile.cs
@@ -0,0 +1,11 @@
+namespace template.Controller
+{
+    public class ControllerFile
+    {
+        public string Test { get; set; }
+        public int Number { get; set; }
+        public ControllerFile()
+        {
+        }
+    }
+}
